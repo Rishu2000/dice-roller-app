95c2d2ba22622869eac910389800300a017e21b5
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-02-16 22:57:44 +0530

    Imported images, added content and added Styling.

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import React from "react"
-import {Text,View,Image,TouchableOpacity} from "react-native"
+import {Text,View,Image,TouchableOpacity,StyleSheet} from "react-native"
+import DiceOne from "./assets/dice1.png"
+import DiceTwo from "./assets/dice2.png"
+import DiceThree from "./assets/dice3.png"
+import DiceFour from "./assets/dice4.png"
+import DiceFive from "./assets/dice5.png"
+import DiceSix from "./assets/dice6.png"
 
 const App = () => {
   return (
     <>
-    <Text>Hello</Text>
+    <View style={styles.container}>
+      <Image source={DiceOne} style={styles.img}/> 
+      <TouchableOpacity>
+    <Text style={styles.content}>Play Again</Text>
+    </TouchableOpacity>
+    </View>
     </>
   )
 }
 
 export default App;
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor:"#0ff"
+  },
+  img:{
+    width:200,
+    height:200
+  },
+  content:{
+    fontSize:20,
+    borderWidth:2,
+    paddingVertical:10,
+    paddingHorizontal:20,
+    marginTop:20,
+    borderRadius:33,
+    fontWeight:"bold"
+  }
+})
